35e9ec88e9652d7387fc297a9f71dca3f06e3be3
Kaus1 <dallabillgates@gmail.com>
2025-10-27 21:04:28 -0700

    Create ImagesDEMO.h

---
+++ End results/ImagesDEMO.h
@@ -0,0 +1,15 @@
+#ifndef IMAGES_H
+#define IMAGES_H
+
+#include "lvgl.h"
+
+// Include all individual image headers
+#include "../images/splash_img.h"
+#include "../images/screen1_img.h"
+#include "../images/screen2_img.h"
+#include "../images/screen3_img.h"
+#include "../images/screen4_img.h"
+#include "../images/dot_img.h"
+#include "../images/stamp_img.h"
+
+#endif
